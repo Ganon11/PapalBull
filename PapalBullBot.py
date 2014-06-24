a0563a2b59ae4ec37d4d3b8d67ef27602c8bf1dd
Michael Stark <stark3@gmail.com>
2014-06-24 14:32:49 -0400

    Whoops.

--- PapalBullBot.py
+++ PapalBullBot.py
@@ -6,12 +6,13 @@ import time
 USERNAME = "Papal_Bull"
 PASSWORD_FILEPATH = os.path.join(os.getcwd(), 'files', 'password.dat')
 COMMENT_PATTERN = re.compile(r'^http://www.reddit.com/r/(?P<subreddit>\w+)/comments/\w+/\w+/\w+$', re.IGNORECASE)
-THREAD_PATTERN = COMMENT_PATTERN = re.compile(r'^http://www.reddit.com/r/(?P<subreddit>\w+)/comments/\w+/\w+/$', re.IGNORECASE)
+THREAD_PATTERN = re.compile(r'^http://www.reddit.com/r/(?P<subreddit>\w+)/comments/\w+/\w+/$', re.IGNORECASE)
 ALLOWED_SUBREDDITS = (
    'Christianity',
    'Sidehugs'
 )
 PAPAL_BULL_MESSAGE = "[papal bull](http://imgur.com/dZBlaTj.gif)"
+USER_AGENT = ("Papal Bull Bot Account by /u/Ganon11 github.com/Ganon11/PapalBull")
 
 def GetPassword():
    password = ''
@@ -20,11 +21,9 @@ def GetPassword():
    PASSWORD_FILE.close()
    return password
 
-user_agent = ("Papal Bull Bot Account by /u/Ganon11 github.com/Ganon11/PapalBull")
-password = GetPassword()
-
 def CheckMessages():
-   r = praw.Reddit(user_agent=user_agent)
+   password = GetPassword()
+   r = praw.Reddit(user_agent=USER_AGENT)
    r.login(USERNAME, password)
 
    for msg in r.get_unread(limit=None):
@@ -42,6 +41,10 @@ def CheckMessages():
          if sub in ALLOWED_SUBREDDITS:
             r.get_submission(whole_url).add_comment(PAPAL_BULL_MESSAGE)
 
-while True:
-   CheckMessages()
-   time.sleep(600) # Sleep for 10 minutes before checking again.
+def DoLoop():
+   while True:
+      CheckMessages()
+      time.sleep(300) # Sleep for 5 minutes before checking again.
+
+if __name__ == '__main__':
+   DoLoop()
